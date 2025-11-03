1e57b0a601a1fae737a641d53af0cae5f8554c0b
kamgacv-ca <kamgacv@gmail.com>
2025-11-03 15:56:58 +0100

    tout

---
+++ get-artifacts1.sh
@@ -0,0 +1,5 @@
+curl \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: Bearer <YOUR-TOKEN>" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+https://api.github.com/repos/OWNER/REPO/actions/artifacts
